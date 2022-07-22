f588d019701308b4dc0078a2b00c1404f6d76e8b
Heet Bhimani <bhimaniheet@gmail.com>
2022-07-22 19:12:10 +0530

    updated : datafile for schema error

--- glue-job/data_init_cow_script.py
+++ glue-job/data_init_cow_script.py
@@ -39,29 +39,38 @@ S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
 #     transformation_ctx="ApplyMapping_node2",
 # )
 
+hudiwriteconfig = {
+    "hoodie.datasource.write.operation":"upsert",
+    'hoodie.datasource.write.table.type': 'COPY_ON_WRITE',
+    "hoodie.datasource.write.recordkey.field": "city_id",
+    "hoodie.datasource.write.recordkey.field": "timestamp",
+    "hoodie.datasource.write.partitionpath.field": "date:SIMPLE",
+    "hoodie.datasource.write.precombine.field": "timestamp",
+    'hoodie.datasource.hive_sync.partition_fields': 'date'
+}
+
+hudidatabaseconfig = {
+    "hoodie.table.name": "wether_data",
+    "hoodie.datasource.hive_sync.table": "wether_data",
+    "hoodie.datasource.hive_sync.enable": "true",
+    "hoodie.datasource.hive_sync.database": "wether_db",
+    'hoodie.datasource.hive_sync.use_jdbc':'false',
+    'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.MultiPartKeysValueExtractor',
+    'hoodie.datasource.write.keygenerator.class': 'org.apache.hudi.keygen.CustomKeyGenerator'
+}
+
+hudiconfig = {
+    'className' : 'org.apache.hudi',
+    "connectionName": "hudi_connector",
+    "path": "s3://hudi-final-demo-hb/final/weather/"
+}
+
+connectionconfig = {**hudiwriteconfig,**hudidatabaseconfig,**hudiconfig}
 # Script generated for node S3 bucket
 S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
     frame=S3bucket_node1,
     connection_type="marketplace.spark",
-    connection_options={
-        'className' : 'org.apache.hudi',
-        "hoodie.table.name": "wether_data",
-        "connectionName": "hudi_connector",
-        "hoodie.datasource.write.operation":"upsert",
-        'hoodie.datasource.write.table.type': 'COPY_ON_WRITE',
-        "path": "s3://hudi-final-demo-hb/final/weather/",
-        "hoodie.datasource.write.recordkey.field": "city_id",
-        "hoodie.datasource.write.recordkey.field": "timestamp",
-        "hoodie.datasource.write.partitionpath.field": "date:SIMPLE",
-        'hoodie.datasource.hive_sync.partition_fields': 'date',
-        "hoodie.datasource.hive_sync.table": "wether_data",
-        "hoodie.datasource.write.precombine.field": "timestamp",
-        "hoodie.datasource.hive_sync.enable": "true",
-        "hoodie.datasource.hive_sync.database": "wether_db",
-        'hoodie.datasource.hive_sync.use_jdbc':'false',
-        'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.MultiPartKeysValueExtractor',
-        'hoodie.datasource.write.keygenerator.class': 'org.apache.hudi.keygen.CustomKeyGenerator'
-    },
+    connection_options=connectionconfig,
     transformation_ctx="S3bucket_node3",
 )
 
